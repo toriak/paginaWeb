5fcfea0451a3a069e3ddba1c67dc3c38f4272223
toriak <raff.94.sma@gmail.com>
2015-06-26 22:39:20 -0300

    intento por tener iconos clase andres 26-06 pagina https://github.com/twbs/bootstrap-sass/issues/653

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Rails4Example
   class Application < Rails::Application
+    # For Bootstrap Sass
+    config.assets.precompile = config.assets.precompile + %w(*.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg *.eot)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
